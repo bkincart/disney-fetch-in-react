31b257afa5d9f7e188fc0735cae6a5cb68dbf275
Brianna Kincart <briannakincart@gmail.com>
2018-09-25 11:46:32 -0400

    remove debugger

--- react/src/components/MovieTile.js
+++ react/src/components/MovieTile.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const MovieTile = props => {
-  debugger;
   let runtime = props.movie.runtime + " minutes"
   let textArray = [props.movie.title, props.movie.release_year, runtime]
   let text = textArray.join(" - ")
